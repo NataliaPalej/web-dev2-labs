f2e72f0762eb81ebd9ba55a679e02fe763220578
Natalia Palej <palejnatalia@gmail.com>
2022-10-24 15:49:40 +0100

    Lab6 Future Value fixed and finished

--- Lab6/2. Future Value/future_value.js
+++ Lab6/2. Future Value/future_value.js
@@ -2,30 +2,48 @@
 const $ = selector => document.querySelector(selector);
 
 const calculateFV = () => {
-    let investment = $("#investment").value;
-    let rate = $("#annual_rate").value;
-    let years = $("#years").value;
-    let result;
-    console.log(`Interest: ${investment}\tRate: ${rate}\tYears: ${years}`);
 
+//taking entered values
+let investment = $("#investment").value;
+let rate = $("#annual_rate").value;
+let years = $("#years").value;
+
+    //error handling
     if (isNaN(investment) || investment < 0 || investment > 10000) {
         $("#investment_error").innerHTML = "Must be > 0 and < 10000";
-    }
+        console.log(`Investment error. Value entered: ${investment}`); }
+    else {
+            $("#investment_error").innerHTML = " ";
+            console.log(`Investment €${investment}`);
+        }
     if (isNaN(rate) || rate < 0 || rate > 12) {
         $("#rate_error").innerHTML = "Must be > 0 and <= 12";
-    }
+        console.log(`Rate error. Value entered: ${rate}`) }
+    else {
+            $("#rate_error").innerHTML = " ";
+            console.log(`Rate: ${rate}%`)
+        }
     if (isNaN(years) || years < 0 || years > 50) {
         $("#years_error").innerHTML = "Must be > 0 and <= 50";
-    }
+        console.log(`Years error. Value entered: ${years}`) }
     else {
-        let i;
-        for (i = 1; i <= years; i++) {
-            result += result*rate/100;
-            result += investment;
+            $("#years_error").innerHTML = " ";
+            console.log(`Years: ${years}`) 
         }
-        $("#future_value").innerHTML = result;
-        console.log("Future value: " + result);
+    
+    //confition to calculate future value
+    if (investment > 0 && investment < 10000 && rate > 0 && rate <= 12 && years > 0 && years < 50)
+    {
+        //calculate future value of an ivestment
+        var x = (1 + (rate/100))
+        var fv = investment*(Math.pow(x, years));
+        fv = fv.toFixed(2);
+        $("#future_value").value = "€" + fv;
+        console.log("Future Value -> €", fv);
+        return fv;
     }
+    
+        
 }
 
 document.addEventListener("DOMContentLoaded", () => {
